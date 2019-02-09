280ab03d9463d485e4aef4628a7e72dd05fa476c
ghkim1223 <joyqplus@gmail.com>
2019-02-09 16:40:23 +0900

    카운트 정보로 변환

--- distribute.py
+++ distribute.py
@@ -2,15 +2,15 @@ import re
 
 
 def distribute(dramList):
-    sortList = [[],[],[]]
+    sortList = {'기묘한 이야기':[], '킹덤':[], '나르코스':[]}
     p1 = re.compile('기묘한[\s|\S]*이야기')
     p2 = re.compile('킹덤[\s|\S]*')
     p3 = re.compile('나르코스[\s|\S]*')
     for drama in dramList:
         if p1.findall(drama):
-            sortList[0].append(drama)
+            sortList['기묘한 이야기'].append(drama)
         elif p2.findall(drama):
-            sortList[1].append(drama)
+            sortList['킹덤'].append(drama)
         elif p3.findall(drama):
-            sortList[2].append(drama)
+            sortList['나르코스'].append(drama)
     return sortList
--- dramaChart.py
+++ dramaChart.py
@@ -11,6 +11,5 @@ dramList = crawling.crawling(datetime.today() - relativedelta(days=1), url)
 # for drama in dramList:
 #     print(drama)
 sortList = distribute.distribute(dramList)
-for sort in sortList:
-    print(sort, len(sort))
-
+for sort in sortList.items():
+    print(sort[0], len(sort[1]))
